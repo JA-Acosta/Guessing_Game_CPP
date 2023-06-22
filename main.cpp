649d4539e4458e210a6f3a4aad5d5896b3801016
JAAR <joe.aaron.acosta@outlook.com>
2023-06-22 12:49:07 -0400

    Version 2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 // JAAR
-// 06/16/2023
+// 06/22/2023
 // Guessing Game
-// Version 1
+// Version 2
 
 
-// Creates an instance of a guessing game:
+// Creates a guessing game where the user can guess a random number that is produced
+// by the program. The user can play several times consecutively and can see their
+// average guesses presented at the end as well as each games stats.
 
 #include <iostream>
 #include <string>
@@ -15,7 +17,7 @@
 
 using namespace std;
 
-// TODO: Use the esc key to exit the program.
+// TODO: Use the (esc) key to exit the program.
 
 struct game_information
 {
@@ -25,38 +27,62 @@ struct game_information
 
 bool is_digit(string guess);
 int check_input();
+void game_instance(int& guess, game_information& guess_data);
 inline void gives_hint(const int& guess, const int& random_number)
-{
-	(guess > random_number) ? cout << "Guess Lower!" : cout <<  "Guess Higher";
-};
+{ (guess > random_number) ? cout << "Guess Lower!" : cout <<  "Guess Higher"; };
 
 int main()
 {
-	int guess;
-	srand((unsigned)time(0));
-	game_information guess_data =
+	vector<game_information*> game_logs;
+	string response;
+	do
 	{
-		0,
-		rand() % 100 + 1
-	};
+		int guess;
+		srand((unsigned)time(0));
+		game_information* game = new game_information
+		{
+			0,
+			rand() % 100 + 1
+		};
+		game_logs.push_back(game);
+		cout << "Guess the Number.\n";
+		cout << game->random_number << "\n";
+		game_instance(guess, *game);
+		cout << "You got it in " << game->total_attemps << "!\n"
+			"Way to go!\n";
+		cout << "Do you want to play again?: ";
+		getline( cin, response );
+	} while ( response == "yes" );
+	int count = 0, total_guesses = 0;
+	/*	!check the address of the data and ensure it is the same as the address above.
+		!check to see if there is an issue printing it out this way in the for loop as a vector.
+	*/
+	for (game_information* game_instance : game_logs)
+	{
+		cout << "For Game #" << count + 1 << " the random number is: " << game_instance->random_number << "\n";
+		cout << "\tYou guessed the right answer in " << game_instance->total_attemps << " tries.\n";
+		total_guesses += game_instance->total_attemps;
+		count++;
+	}
+	cout << "You played a total of " << count << " games today with an average of "
+		<< (double) total_guesses / count << '\n';
+	cout << "DONE!";
+}
 
-	// Introduction:
-	cout << "Guess the Number.\n";
-
-	// Instance of game:
+// Generates a single instance of the guessing game.
+// Param: int& guess, game_information& game
+void game_instance(int& guess, game_information& game)
+{
 	do
 	{
 		cout << "Enter your guess: ";
 		guess = check_input();
-		if (guess != guess_data.random_number){
-		gives_hint(guess, guess_data.random_number);
+		if (guess != game.random_number) {
+			gives_hint(guess, game.random_number);
 		}
 		cout << endl;
-		guess_data.total_attemps++;
-	} while (guess != guess_data.random_number);
-	cout << "You got it in " << guess_data.total_attemps << "!\n"
-		"Way to go!";
-	cout << "DONE!";
+		game.total_attemps++;
+	} while (guess != game.random_number);
 }
 
 // Gets an input from the user and if it's a number returns it otherwise, it prompts
